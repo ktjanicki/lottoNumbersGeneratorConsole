d02bce2866b4ed857b5d1e327ab7fbae3c1ea190
Krzysztof Janicki <ktjanicki@gmail.com>
2024-12-20 16:30:50 +0100

    create showSimulationProgress function and code reformatting

--- lottoNumbersGeneratorConsole.js
+++ lottoNumbersGeneratorConsole.js
@@ -1,7 +1,12 @@
 const lottoDraws = (numberOfDraws, numberOfResults) => {
   const drawResults = new Object();
   const everyOneProcent = Array.from({ length: 100 }, (_, i) => Math.floor((i + 1) * 0.01 * numberOfDraws));
-  const formatNumber = number => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' '); 
+  const formatNumber = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+
+  const showSimulationProgress = (draw) => {
+    console.clear();
+    console.log(`\nSymulacja w toku: ${((draw / numberOfDraws) * 100).toFixed()}%`);
+  };
 
   const drawNumbers = () => {
     const numbers = new Set();
@@ -9,23 +14,24 @@ const lottoDraws = (numberOfDraws, numberOfResults) => {
       numbers.add(Math.floor(Math.random() * 49 + 1));
     }
 
-    return [...numbers].sort((a, b) => a - b).join(', ');
+    return [...numbers].sort((a, b) => a - b).join(", ");
   };
 
-  for(let i = 0; i < numberOfDraws; i++){
+  for (let i = 0; i < numberOfDraws; i++) {
     const draw = drawNumbers();
-    drawResults[draw] ? drawResults[draw]++ : drawResults[draw] = 1;
-    if (everyOneProcent.includes(i + 1)) {
-      console.clear()
-      console.log(`\nSymulacja w toku: ${((i / numberOfDraws) * 100).toFixed()}%`);
-    }
+    drawResults[draw] ? drawResults[draw]++ : (drawResults[draw] = 1);
+    if (everyOneProcent.includes(i + 1)) showSimulationProgress(i + 1);
   }
 
   const sortedNumbers = Object.keys(drawResults).sort((a, b) => drawResults[b] - drawResults[a]);
-    console.log(`\nWykonano ${formatNumber(numberOfDraws)} losowań w których padło ${formatNumber(Object.keys(drawResults).length)} unikalnych zestawów liczb.\nNajczęściej padały zestawy liczb:\n`);
-    sortedNumbers.slice(0, numberOfResults).forEach((numbers, i) => {
-      console.log(`${i + 1}: ${numbers}: ${drawResults[numbers]} razy.`)
-    });
-}
+  console.log(
+    `\nWykonano ${formatNumber(numberOfDraws)} losowań w których padło ${formatNumber(
+      Object.keys(drawResults).length
+    )} unikalnych zestawów liczb.\nNajczęściej padały zestawy liczb:\n`
+  );
+  sortedNumbers.slice(0, numberOfResults).forEach((numbers, i) => {
+    console.log(`${i + 1}: ${numbers}: ${drawResults[numbers]} razy.`);
+  });
+};
 
 lottoDraws(5_000_000, 6);
